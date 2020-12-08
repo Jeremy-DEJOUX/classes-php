ebbf21075ad40a52695efa5f84446b79f6ec43cf
Jeremy DEJOUX <jeremy.dejoux@laplateforme.io>
2020-12-08 16:22:42 +0100

    création de la classe user et des méthodes

--- configs/user.php
+++ configs/user.php
@@ -1,5 +1,64 @@
 <?php
 
+  class User{
+    private $id;
+    public $login;
+    public $email;
+    public $firstname;
+    public $lastname;
 
+    public function register($login, $password, $email, $firstname, $lastname){
+
+    }
+
+    public function connect($login, $password){
+
+    }
+
+
+    public function disconnect(){
+
+    }
+
+
+    public function delete(){
+
+    }
+
+    public function update($login, $password, $email, $firstname, $lastname){
+
+    }
+
+
+    public function isConnected(){
+
+    }
+
+    public function getAllInfos(){
+
+    }
+
+    public function getLogin(){
+
+    }
+
+    public function getEmail(){
+
+    }
+
+    public function getFirstname(){
+
+    }
+
+    public function getLastname(){
+
+    }
+
+    public function refresh(){
+
+    }
+
+
+  }
 
 ?>
