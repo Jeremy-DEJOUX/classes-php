5116a135ee504aebc25400b82d8fc87eb9fb625f
Jeremy DEJOUX <jeremy.dejoux@laplateforme.io>
2020-12-11 11:38:11 +0100

    Mise en place JOB 3

---
+++ configs/lpdo.php
@@ -0,0 +1,30 @@
+<?php
+  class lpdo{
+
+
+
+
+
+
+    function constructeur($host, $username, $password, $db){}
+
+    function connect($host, $username, $password, $db){}
+
+    function destructeur(){}
+
+    function close(){}
+
+    function execute($query){}
+
+    function getLastQuery(){}
+
+    function getLastResult(){}
+
+    function getTables(){}
+
+    function getFields($table){}
+
+
+  }
+
+ ?>
